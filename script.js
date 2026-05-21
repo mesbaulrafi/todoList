efc051e4416d84413206080d857b21e4f33934b0
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-05-22 00:12:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let inputBox = document.getElementById("inputBox")
+let todoList = document.getElementById("todoList");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("Plse add")
+    } else {
+        let li = document.createElement('li')
+        li.innerHTML = inputBox.value
+        todoList.appendChild(li)
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+        inputBox.value = ""
+        saveData()
+    }
+}
+
+
+ todoList.addEventListener('click',(e)=>{
+    if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        saveData()
+    }
+
+    
+ } , false)
+
+
+ function saveData() {
+    localStorage.setItem("data", todoList.innerHTML)
+ }
+
+ function showData() {
+    todoList.innerHTML = localStorage.getItem("data")
+ }
+ showData()
